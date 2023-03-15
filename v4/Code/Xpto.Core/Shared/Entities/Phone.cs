0008ee86c0f5d9017c2766b6e5dd4882e0137c6d
Uilan Coqueiro <uilan@ajinsoft.com.br>
2023-03-15 19:42:30 -0300

    2023-03-15

---
+++ v4/Code/Xpto.Core/Shared/Entities/Phone.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Reflection.Emit;
+
+namespace Xpto.Core.Shared.Entities
+{
+    public class Phone
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public int Ddd { get; set; }
+        public long Number { get; set; }
+        public string Note { get; set; }
+
+        public Phone()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override string ToString()
+        {
+            return $"({Ddd}) {Number}";
+        }
+    }
+}
---
+++ v4/Code/Xpto.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Xpto.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
